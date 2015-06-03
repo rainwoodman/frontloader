c8bd2a1089d75256f7c5e1c49e07a81b4b5b1cb3
Yu Feng <yfeng1@berkeley.edu>
2015-06-02 22:06:18 -0700

    faster numpy based indexing.

---
+++ frontloader/tinydb/npindex.py
@@ -0,0 +1,26 @@
+import numpy
+
+class Index(object):
+    """ Frozen fast indices of tinydb tables, via numpy 
+
+        Only indexing on one key is supported.
+    """
+
+    def __init__(self, table, key, **kwargs):
+        self.table = table
+        self.key = key
+        self.reverse = bool(kwargs.get('reverse'))
+
+        self.key_values = numpy.array([d[key] for d in self.table.all()])
+        eids = numpy.array([d.eid for d in self.table.all()]) 
+        arg = numpy.argsort(self.key_values)
+        self.eids = eids[arg]
+        self.sorted = self.key_values[arg]
+
+    def contains(self, value):
+        if len(self.sorted) == 0:
+            return value != value
+        ind = self.sorted.searchsorted(value)
+        ind = numpy.clip(ind, 0, len(self.sorted) - 1)
+        return self.sorted[ind] == value
+
